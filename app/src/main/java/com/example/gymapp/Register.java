6f61d62ecea3c0aaa701ee4d6e68c809038ada49
Martin <martinifc96@outlook.com>
2021-03-25 01:24:11 +0000

    Registration method verifies entered email address isnt already taken

--- app/src/main/java/com/example/gymapp/Register.java
+++ app/src/main/java/com/example/gymapp/Register.java
@@ -3,6 +3,7 @@ package com.example.gymapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,6 +14,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,9 +38,56 @@ public class Register extends AppCompatActivity {
         editTextPass = findViewById(R.id.passEntry2);
     }
 
+    public void emailAvailability(View v){
+
+        final String emailEntry = editTextEmail.getText().toString();
+        final String passEntry = editTextPass.getText().toString();
+
+
+        usersRef.whereEqualTo("email", emailEntry)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+
+                            // String email = documentSnapshot.getString(KEY_EMAIL);
+
+
+                        }
+                        if(queryDocumentSnapshots.isEmpty()){
+                            String email = editTextEmail.getText().toString();
+                            String pass = editTextPass.getText().toString();
+
+                            Map<String, Object> user = new HashMap<>();
+                            user.put(KEY_EMAIL, email);
+                            user.put(KEY_PASS, pass);
+
+                            usersRef.add(user);
+
+                            Toast.makeText(Register.this, "work", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(Register.this, "not work", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+    }
+
     public void register(View v){
+
         String email = editTextEmail.getText().toString();
         String pass = editTextPass.getText().toString();
+
 
         Map<String, Object> user = new HashMap<>();
         user.put(KEY_EMAIL, email);
@@ -46,9 +96,6 @@ public class Register extends AppCompatActivity {
 
 
         usersRef.add(user);
-
-
-
 
     }
 }
