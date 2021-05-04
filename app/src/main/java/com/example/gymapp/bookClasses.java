9b18f34f92eac9d9c971dfbd85fe721e5bddcd30
Martin <martinifc96@outlook.com>
2021-05-04 21:07:00 +0100

    Added querying functionality using spinner for gym class filtering based on category

--- app/src/main/java/com/example/gymapp/bookClasses.java
+++ app/src/main/java/com/example/gymapp/bookClasses.java
@@ -6,7 +6,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -21,9 +26,9 @@ import java.util.ArrayList;
 
 import javax.annotation.Nullable;
 
-public class bookClasses extends AppCompatActivity {
+public class bookClasses extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private classesAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     ArrayList<classData> classList = new ArrayList<>();
 
@@ -37,6 +42,12 @@ public class bookClasses extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_classes);
         testText = findViewById(R.id.testText);
+        Spinner spinner = findViewById(R.id.spinnerClasses);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.classCats, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
 
       //  loadAll();
@@ -49,10 +60,64 @@ public class bookClasses extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
+    }
 
+    @Override // on spinner GymClass option chosen
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+        Toast.makeText(parent.getContext(), "item chosen" , Toast.LENGTH_SHORT);
 
+        String text = parent.getItemAtPosition(position).toString();
+        System.out.println("Spinner activated: " + text);
 
+        loaders(text);
+    }
 
+    public void loaders(String s){
+
+        classList.clear();
+
+        classRef.whereEqualTo("name", s).addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(e != null){
+                    return;
+                }
+
+                String data = "";
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+
+                    classData classData = documentSnapshot.toObject(com.example.gymapp.classData.class);
+
+                    String name = classData.getName();
+                    String date = classData.getDate();
+                    String time = classData.getTime();
+                    String length = classData.getLength();
+                    int capacity = classData.getCapacity();
+                    int attendees = classData.getAttendees();
+
+                    data += "Name: " + name + "\nDate: " +  date + "\nTime " + time + "\nLength: " + length
+                            + "\nCapacity: " + capacity + "\nAttendees: " + attendees;
+
+                    //System.out.println(data);
+                    classList.add(new classData(name, date, time, length, capacity, attendees));
+                    testText.setText(data);
+
+                }
+
+                mAdapter = new classesAdapter(classList);
+
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                mRecyclerView.setAdapter(mAdapter);
+                mAdapter.setOnItemClickListener(new classesAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(int position) {
+
+                        System.out.println("Position: " + position);
+                    }
+                });
+            }
+        });
+        System.out.println("array at end of on start method: " + classList);
 
     }
 
@@ -91,51 +156,73 @@ public class bookClasses extends AppCompatActivity {
 
                 mRecyclerView.setLayoutManager(mLayoutManager);
                 mRecyclerView.setAdapter(mAdapter);
+                mAdapter.setOnItemClickListener(new classesAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(int position) {
+
+                        System.out.println("Position: " + position);
+                    }
+                });
             }
         });
         System.out.println("array at end of on start method: " + classList);
     }
 
-    public void loadAll(){
-        classList = new ArrayList<>();
+    public void loadAll(View v){
 
-        classRef.get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        classList.clear();
+
+        classRef.whereEqualTo("name", "running").addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(e != null){
+                    return;
+                }
+
+                String data = "";
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+
+                    classData classData = documentSnapshot.toObject(com.example.gymapp.classData.class);
+
+                    String name = classData.getName();
+                    String date = classData.getDate();
+                    String time = classData.getTime();
+                    String length = classData.getLength();
+                    int capacity = classData.getCapacity();
+                    int attendees = classData.getAttendees();
+
+                    data += "Name: " + name + "\nDate: " +  date + "\nTime " + time + "\nLength: " + length
+                            + "\nCapacity: " + capacity + "\nAttendees: " + attendees;
+
+                    //System.out.println(data);
+                    classList.add(new classData(name, date, time, length, capacity, attendees));
+                    testText.setText(data);
+
+                }
+
+                mAdapter = new classesAdapter(classList);
+
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                mRecyclerView.setAdapter(mAdapter);
+                mAdapter.setOnItemClickListener(new classesAdapter.OnItemClickListener() {
                     @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        String data = "";
+                    public void onItemClick(int position) {
 
-                        for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
-                            classData classData = documentSnapshot.toObject(classData.class);
-
-                            String name = classData.getName();
-                            String date = classData.getDate();
-                            String time = classData.getTime();
-                            String length = classData.getLength();
-                            int capacity = classData.getCapacity();
-                            int attendees = classData.getAttendees();
-
-                            data += "Name: " + name + "\nDate: " +  date + "\nTime " + time + "\nLength: " + length
-                                    + "\nCapacity: " + capacity + "\nAttendees: " + attendees;
-
-                            // test if data string populated properly
-                            System.out.println(data);
-
-                            classList.add(new classData(name, date, time, length, capacity, attendees));
-                            System.out.println("array in for loop: " + classList);
-                        }
-                        // test if array list is being populated correct
-                        System.out.println("array outside for loop: " + classList);
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
+                        System.out.println("Position: " + position);
                     }
                 });
-        System.out.println("array at end of load method: " + classList);
+            }
+        });
+        System.out.println("array at end of on start method: " + classList);
+
+
+
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){
+
+    }
+
 
 }
