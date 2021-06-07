fe73e74d672e7d6d751bbf428b90590938845465
Martin <martinifc96@outlook.com>
2021-06-07 13:42:47 +0100

    spinners and load workout method / button functionality

--- app/src/main/java/com/example/gymapp/tabworkout.java
+++ app/src/main/java/com/example/gymapp/tabworkout.java
@@ -11,7 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
@@ -38,7 +41,12 @@ public class tabworkout extends Fragment {
     private RecyclerView mRecyclerView;
     private workoutAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private Button loadBtn;
     ArrayList<exerciseData> exerciseList = new ArrayList<>();
+    String dateEntry;
+    String daytext;
+    String monthtext;
+    String yeartext;
 
 
 
@@ -56,16 +64,124 @@ public class tabworkout extends Fragment {
         mRecyclerView = view.findViewById(R.id.workoutList);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(getActivity());
-
-
-
-
-        loadQuery("03/06/2021");
+        loadBtn = view.findViewById(R.id.loadWorkoutBtn);
 
 
 
 
 
+
+
+        // ----------------------------------- 3 DATE SELECTION SPINNERS ------------------------------------------------------------------------------
+        Spinner spinnerDay = view.findViewById(R.id.daySpinner);
+        Spinner spinnerMonth = view.findViewById(R.id.monthSpinner);
+        Spinner spinnerYear = view.findViewById(R.id.yearSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.dayNos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDay.setAdapter(adapter);
+        spinnerDay.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                daytext = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this.getActivity(), R.array.months, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMonth.setAdapter(adapter2);
+        spinnerMonth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String holder = parent.getItemAtPosition(position).toString();
+                switch(holder){
+                    case "Jan":
+                        monthtext = "1";
+                        break;
+
+                    case "Feb":
+                        monthtext = "2";
+                        break;
+
+                    case "March":
+                        monthtext = "3";
+                        break;
+
+                    case "Apr":
+                        monthtext = "4";
+                        break;
+
+                    case "May":
+                        monthtext = "5";
+                        break;
+
+                    case "Jun":
+                        monthtext = "6";
+                        break;
+
+                    case "Jul":
+                        monthtext = "7";
+                        break;
+
+                    case "Aug":
+                        monthtext = "8";
+                        break;
+
+                    case "Sep":
+                        monthtext = "9";
+                        break;
+
+                    case "Oct":
+                        monthtext = "10";
+                        break;
+
+                    case "Nov":
+                        monthtext = "11";
+                        break;
+
+                    case "Dec":
+                        monthtext = "12";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this.getActivity(), R.array.years, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerYear.setAdapter(adapter3);
+        spinnerYear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                yeartext = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        // ----------------------------------------------------------------------------------------------------------------------------------------
+
+        loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exerciseList.clear();
+                dateEntry = daytext + "/" + monthtext + "/" + yeartext;
+                System.out.println("completed datentry: " + dateEntry);
+                loadQuery(dateEntry);
+            }
+        });
 
 
 
